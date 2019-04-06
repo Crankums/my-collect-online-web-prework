c443978d4e1cdb675cce6bc320ce281158488cda
Craig Donavin <crdonavin@gmail.com>
2019-04-06 02:18:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   new_arr = []
   counter = 0
   while counter<array.length
-    array.each do |i|
-      new_arr << yield(i)
+      new_arr << yield(arraty[counter])
 
     end
 
