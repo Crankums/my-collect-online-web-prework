ddce947aa08d24cb29e08499bfe9457a6db2772c
Craig Donavin <crdonavin@gmail.com>
2019-04-06 02:00:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  new_arr = []
+  counter = 0
+  while counter<array.length
+    array.each do |i|
+      new_arr << i
+      end
+    counter += 1
+  end
+  new_arr
+end
