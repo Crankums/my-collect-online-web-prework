3bb84b31f1e8c2f2c141780b82b130b8a8124fe1
Craig Donavin <crdonavin@gmail.com>
2019-04-06 02:07:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   new_arr = []
   counter = 0
   while counter<array.length
-    yield array[counter]
-    new_arr <<
+    new_arr << yield array[counter]
     counter += 1
   end
   new_arr
